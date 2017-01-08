d5208fec3681936f938e1051eb40663bba96d55c
hongyukeji <huan1314!>
2017-01-08 17:26:00 +0800

    eBestMall

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -10,14 +10,17 @@ use yii\web\AssetBundle;
 class AppAsset extends AssetBundle
 {
     public $basePath = '@webroot';
-    public $baseUrl = '@web';
+    public $baseUrl = '@web/static';
     public $css = [
-        'css/site.css',
+        'css/style.css',
+
     ];
     public $js = [
+        'js/jquery-1.8.2.min.js',
+        'js/main.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        //'yii\bootstrap\BootstrapAsset',
     ];
 }
